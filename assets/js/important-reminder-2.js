d4534dc0d0886f984eb4e0a7d802fe78fae3d2e7
Luca Nioi <lucahnioi@gmail.com>
2024-08-28 03:35:42 +0200

    Edit links

--- assets/js/important-reminder-2.js
+++ assets/js/important-reminder-2.js
@@ -1,14 +1,14 @@
-document.getElementById('ticketLink').style.pointerEvents = 'none' // Disable link by default
-document.getElementById('ticketLink').style.opacity = '0.5' // Make the link look disabled
+document.getElementById('final-ticket-link').style.pointerEvents = 'none' // Disable link by default
+document.getElementById('final-ticket-link').style.opacity = '0.1' // Make the link look disabled
 
 document.getElementById('agree').addEventListener('change', function () {
-  const link = document.getElementById('ticketLink')
+  const link = document.getElementById('final-ticket-link')
   if (this.checked) {
     link.style.pointerEvents = 'auto' // Enable the link
     link.style.opacity = '1' // Make the link fully visible
   } else {
     link.style.pointerEvents = 'none' // Disable the link
-    link.style.opacity = '0.5' // Make the link look disabled
+    link.style.opacity = '0.1' // Make the link look disabled
   }
 })
 
